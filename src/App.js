d75dcea8982b44c3c1ae89ed20e2d00528bf3110
cubasve <eva.cubasvasquez@gmail.com>
2021-08-14 11:24:19 -0400

    Can add, delete and retrieve nominations from local storage now

--- src/App.js
+++ src/App.js
@@ -1,127 +1,134 @@
-import { useCallback, useEffect, useState } from 'react';
-import './App.css';
-import SearchBar from './components/Search/Search';
-import Nominations from './components/Nominations/Nominations';
-import Movies from './components/Movies/Movies';
+import { useCallback, useEffect, useState } from "react";
+import "./App.css";
+import SearchBar from "./components/Search/Search";
+import Nominations from "./components/Nominations/Nominations";
+import Movies from "./components/Movies/Movies";
 
 export default function App() {
-  const [searchQuery, setSearchQuery] = useState('');
-  const [movieList, setMovieList] = useState([ ]);
-  const [nominations, setNominations] = useState([ ]);
-  const [errorMessage, setErrorMessage] = useState('');
-  const [isLoading, setIsLoading] = useState(true);
+	const [searchQuery, setSearchQuery] = useState("");
+	const [movieList, setMovieList] = useState([]);
+	const [nominations, setNominations] = useState([]);
+	const [errorMessage, setErrorMessage] = useState("");
+	const [isLoading, setIsLoading] = useState(true);
 
-  const SECRET = process.env.REACT_APP_OMDB_API_KEY;
-  const movieURL = `https://www.omdbapi.com/?apikey=${SECRET}&s=${searchQuery}`;
+	const SECRET = process.env.REACT_APP_OMDB_API_KEY;
+	const movieURL = `https://www.omdbapi.com/?apikey=${SECRET}&s=${searchQuery}`;
 
-  const fetchNominationsFromStorage = useCallback(() => {
-    //JSON.parse(): converts items in local storage from a string to an object
-    const nominationsFromStorage = JSON.parse(localStorage.getItem('savedNominations') || '[]');
-    setNominations(nominationsFromStorage);
-  }, [nominations, setNominations]);
+	const fetchNominationsFromStorage = useCallback(() => {
+		//JSON.parse(): converts items in local storage from a string to an object
+		const storedNominations = localStorage.getItem("savedNominations");
+		if (storedNominations) {
+			setNominations(JSON.parse(storedNominations));
+		}
+	}, [setNominations]);
 
-  useEffect(() => {
-    if (isLoading) {
-      fetchNominationsFromStorage();
-      setIsLoading(false);
-    }
-  }, [fetchNominationsFromStorage, isLoading]);
+	useEffect(() => {
+		// if (isLoading) {
+		fetchNominationsFromStorage();
+		// 	setIsLoading(false);
+		// }
+	}, [fetchNominationsFromStorage /*isLoading*/]);
 
-  useEffect(() => {
-    (async () => {
-        try {
-          const response = await fetch(movieURL);
-          const data = await response.json();
+	useEffect(() => {
+		(async () => {
+			try {
+				const response = await fetch(movieURL);
+				const data = await response.json();
 
-          if (Array.isArray(data.Search)) {
-            //Only movies appear in the search results
-            let onlyMovies = data.Search.filter(result => result.Type === 'movie');
+				if (Array.isArray(data.Search)) {
+					//Only movies appear in the search results
+					let onlyMovies = data.Search.filter(
+						(result) => result.Type === "movie"
+					);
 
-            //Movies with unique imdbID will be shown (duplicates are filtered out)
-            let noDuplicateMovies = onlyMovies.filter((id,index, onlyMovies)=>onlyMovies.findIndex(movie=>(movie.imdbID === id.imdbID))===index)
-            
-            setMovieList(noDuplicateMovies);
-          } else {
-            if (data.Error !== "Incorrect IMDb ID.") {
-              setErrorMessage(data.Error);
-            }
-          }
-        } catch (err) {
-          console.error(err);
-        }
-      })();
-  }, [searchQuery, movieList, movieURL]);
+					//Movies with unique imdbID will be shown (duplicates are filtered out)
+					let noDuplicateMovies = onlyMovies.filter(
+						(id, index, onlyMovies) =>
+							onlyMovies.findIndex((movie) => movie.imdbID === id.imdbID) ===
+							index
+					);
 
-  const handleSearchQueryChange = async (e) => {
-    e.preventDefault();
-    console.log('target.value: ', e.target.value);
-    console.log(typeof e.target.value)
-    setMovieList([]);
-    setSearchQuery(e.target.value);
-  }
+					setMovieList(noDuplicateMovies);
+				} else {
+					if (data.Error !== "Incorrect IMDb ID.") {
+						setErrorMessage(data.Error);
+					}
+				}
+			} catch (err) {
+				console.error(err);
+			}
+		})();
+	}, [searchQuery, movieList, movieURL]);
 
-  const handleSearchQuerySubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await fetch(movieURL);
-      const data = await response.json();
-      setMovieList(data.Search);
-    } catch (err) {
-      console.error(err);
-    }
-  }
+	const handleSearchQueryChange = async (e) => {
+		e.preventDefault();
+		console.log("target.value: ", e.target.value);
+		console.log(typeof e.target.value);
+		setMovieList([]);
+		setSearchQuery(e.target.value);
+	};
 
-  const handleAddNomination = (movie) => {
-    console.log('movie: ', movie);
-    console.log('nominations: ', nominations);
-    setNominations(nominations => [...nominations, movie]);
+	const handleSearchQuerySubmit = async (e) => {
+		e.preventDefault();
+		try {
+			const response = await fetch(movieURL);
+			const data = await response.json();
+			setMovieList(data.Search);
+		} catch (err) {
+			console.error(err);
+		}
+	};
 
-    //JSON.stringify(): converts a JS object/value to a string
-    localStorage.setItem('savedNominations', JSON.stringify([...nominations]));
-    console.log('localStorage: ', localStorage.getItem('savedNominations'));
-    console.log('nominations after adding to LocalS: ', nominations)
-  }
+	const handleAddNomination = (movie) => {
+		const newNominations = [...nominations, movie];
+		setNominations((nominations) => [...nominations, movie]);
+		//JSON.stringify(): converts a JS object/value to a string
+		localStorage.setItem("savedNominations", JSON.stringify(newNominations));
+	};
 
-  const handleDeleteNomination = (e) => {
-    const nominationsCopy = [...nominations];
-    let index = nominationsCopy.findIndex(movie => movie.imdbID === e.target.value);
+	const handleDeleteNomination = (e) => {
+		const nominationsCopy = [...nominations];
+		let index = nominationsCopy.findIndex(
+			(movie) => movie.imdbID === e.target.value
+		);
+		if (index !== -1) {
+			nominationsCopy.splice(index, 1);
+			setNominations(nominationsCopy);
+			localStorage.setItem("savedNominations", JSON.stringify(nominationsCopy));
+		}
+		// let filteredNominations = nominations.filter(
+		// 	(movie) => movie.imdbID !== e.target.value
+		// );
+		// setNominations(filteredNominations);
+		// localStorage.setItem(
+		// 	"savedNominations",
+		// 	JSON.stringify(filteredNominations)
+		// );
+	};
 
-    if (index !== -1) {
-      nominationsCopy.splice(index , 1);
-      setNominations(nominationsCopy);
-      console.log('delete nominations Copy: ', nominationsCopy);
-
-      localStorage.setItem('savedNominations', JSON.stringify([nominationsCopy]));
-      console.log('localStorage: ', localStorage.getItem('savedNominations'));
-      console.log('localS delete nomination: ', nominations);
-      //localStorage.setItem('savedNominations', JSON.stringify([...nominationsCopy]));
-
-      // localStorage.clear();
-      // localStorage.setItem('savedNominations', JSON.stringify([nominations]));
-      // console.log(localStorage)
-    }
-  }
-
-  return (
-    <div className='App' data-testid='app'>
-      <h1>The Shoppies</h1>
-      <SearchBar 
-        handleSearchQueryChange={handleSearchQueryChange}
-        handleSearchQuerySubmit={handleSearchQuerySubmit}
-        searchQuery={searchQuery} 
-      />
-      <div className="shoppies">
-        <Movies 
-          errorMessage={errorMessage}
-          handleAddNomination={handleAddNomination}
-          movieList={movieList} 
-          nominations={nominations}
-        />
-        <Nominations 
-          handleDeleteNomination={handleDeleteNomination}
-          nominations={nominations}
-        />
-      </div>
-    </div>
-  );
+	return (
+		<div className="App" data-testid="app">
+			<h1>The Shoppies</h1>
+			<SearchBar
+				data-testid="search"
+				handleSearchQueryChange={handleSearchQueryChange}
+				handleSearchQuerySubmit={handleSearchQuerySubmit}
+				searchQuery={searchQuery}
+			/>
+			<div className="shoppies">
+				<Movies
+					data-testid="movies"
+					errorMessage={errorMessage}
+					handleAddNomination={handleAddNomination}
+					movieList={movieList}
+					nominations={nominations}
+				/>
+				<Nominations
+					data-testid="nominations"
+					handleDeleteNomination={handleDeleteNomination}
+					nominations={nominations}
+				/>
+			</div>
+		</div>
+	);
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,52 @@
-import { render, screen, cleanup } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen, cleanup } from "@testing-library/react";
+import App from "./App";
+// import Movies from '../src/components/Movies/Movies';
+// import Search from '../src/components/Search/Search';
 
 afterEach(() => {
-  cleanup();
-  //Cleans up after every test
-  //Makes sure every test has the same starting point
+	cleanup();
+	//Cleans up after every test
+	//Makes sure every test has the same starting point
 });
 
-test('test', () => {
-  render(<App />);
-  const appElement = screen.getByTestId('app');
-  expect(appElement).toBeInTheDocument();
+// test("renders learn react link", () => {
+// 	render(<App />);
+// 	const linkElement = screen.getByText(/learn react/i);
+// 	expect(linkElement).toBeInTheDocument();
+// });
+
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<button></button>, div);
+// })
+
+// it("search query is a string and >1 character", () => {
+// 	expect(App.handleSearchQueryChange("Lord")).toBe("Lord");
+// });
+
+it("app.js contains search, movies and nominations", () => {
+	const { queryByTestId } = render(<App />);
+	expect(queryByTestId("search")).toBe(null);
+	// expect(queryByTestId("movies")).toBe(<div />);
+	// expect(queryByTestId("nominations")).toBe(null);
 });
 
+// test("test", () => {
+// 	render(<App />);
+// 	const appElement = screen.getByTestId("app");
+// 	expect(appElement).toBeInTheDocument();
+// 	// expect(appElement).toContain()
+// });
+
+// test('search query is a string and > 1 character', () => {
+//   render(<Movies />);
+//   const movieElement = screen.getByTestId('movies');
+//   expect(movieElement).toBeInTheDocument();
+// });
+
+// test("search query is a string and > 1 character", () => {
+// 	render(<Search />);
+// 	const searchQueryElement = screen.getByTestId("search-query");
+// 	expect(searchQueryElement).toBe("string");
+// });
